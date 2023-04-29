8d7580e52a480cc88cb7a3ef983d72287bc9a732
SummerD-max <862677188@qq.com>
2023-04-29 13:00:59 +0800

    add blogcatalog_WKAFL.py

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-import torch
-x = torch.randn(3, 4)
-print(x)
-res, indices = torch.sort(x)
-
-print(res)
-print(indices)
-
-res, indices = torch.sort(x, 0)
-print(res)
-print(indices)
-x = torch.tensor([0, 1] * 9)
-x.sort()
-x.sort(stable=True)
+print("hello")
